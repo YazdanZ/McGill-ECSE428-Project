ed9a7efb68fdc9fc55db057cef27fa36b1df7ad2
himelsaha29 <himelsaha29@live.com>
2023-02-10 22:26:24 -0500

    Signup page modified

--- frontend/src/components/pages/Signup.jsx
+++ frontend/src/components/pages/Signup.jsx
@@ -1,45 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import ParticlesComponent from '../Particles'
 
 import '../../App.css'
 
 export default function Signup() {
 
     return (
-        <div className="title">
-            <h2>Create an account</h2>
-            <form className='form'>
-                <p>
-                    <label>Name</label><br/>
-                    <input type="text" name="name" required /><br/>
-                    <label></label><br/>
-                </p>
-                <p>
-                    <label>Email address</label><br/>
-                    <input type="email" name="email" required /><br/>
-                    <label></label><br/>
-                </p>
-                <p>
-                    <label>McGill ID</label><br/>
-                    <input type='text' name="id" required /><br/>
-                    <label></label><br/>
-                </p>
-                <p>
-                    <label>Password</label><br/>
-                    <input type="password" name="password" required /><br/>
-                    <label></label><br/>
-                </p>
-                <p>
-                    <input type="checkbox" name="checkbox" id="checkbox" /> <span>I am a driver</span><br/>
-                    <label></label><br/>
-                </p>
-                <p>
-                    <button id="sub_btn" type="submit">Register</button>
-                </p>
-            </form>
-            <footer>
-                <p>Already have an account?</p>
-            </footer>
+        <div className='App-header'>
+            <ParticlesComponent/>
+            <div className="title">
+                <h2>Create an account</h2>
+                <form className='form'>
+                    <p>
+                        <label>Name</label><br/>
+                        <input type="text" name="name" required /><br/>
+                        <label></label><br/>
+                    </p>
+                    <p>
+                        <label>Email address</label><br/>
+                        <input type="email" name="email" required /><br/>
+                        <label></label><br/>
+                    </p>
+                    <p>
+                        <label>McGill ID</label><br/>
+                        <input type='text' name="id" required /><br/>
+                        <label></label><br/>
+                    </p>
+                    <p>
+                        <label>Password</label><br/>
+                        <input type="password" name="password" required /><br/>
+                        <label></label><br/>
+                    </p>
+                    <p>
+                        <input type="checkbox" name="checkbox" id="checkbox" /> <span>I am a driver</span><br/>
+                        <label></label><br/>
+                    </p>
+                    <p>
+                        <button id="sub_btn" type="submit">Register</button>
+                    </p>
+                </form>
+                <footer>
+                    <p>Already have an account?</p>
+                </footer>
+            </div>
         </div>
     )
 
