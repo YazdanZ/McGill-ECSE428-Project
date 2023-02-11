813d4fb3fbe2028b1dcec69041f9a858da27e242
himelsaha29 <himelsaha29@live.com>
2023-02-10 19:23:23 -0500

    Signup page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Signup from './components/pages/Signup'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css'
+
+export default function App() {
+    return (
+      <div className='App'>
+        <Signup/>
+        <Footer/>
+      </div>
+    )
 }
 
-export default App;
+const Footer = () => {
+  return (
+      <p className="text-center" style={ FooterStyle }>Made by McGillians, for McGillians</p>
+  )
+}
+
+const FooterStyle = {
+  background: "#222",
+  fontSize: ".8rem",
+  color: "#fff",
+  position: "absolute",
+  bottom: 0,
+  padding: "1rem",
+  margin: 0,
+  width: "100%",
+  opacity: ".5"
+}
+
