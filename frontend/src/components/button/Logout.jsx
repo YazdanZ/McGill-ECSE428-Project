64d2633ffa3a7c91e324398a29d2140179643521
samiad2788 <52794206+sami270@users.noreply.github.com>
2023-02-16 15:02:12 -0400

    Added routing

--- frontend/src/components/button/Logout.jsx
+++ frontend/src/components/button/Logout.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import ParticlesComponent from '../Particles'
-import Userfront from "@userfront/core";
 
 import '../../App.css'
 
 export default function Logout() {
 
     return (
+
         <p>
             <button id="logout_btn" type="submit">
-                Log out
+                <Link to="/">Log out</Link>
             </button>
         </p>
     )
