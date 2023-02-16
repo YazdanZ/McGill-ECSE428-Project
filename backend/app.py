b3e3f58bf9f6025bdb71e4713fda8e381abce68d
samiad2788 <52794206+sami270@users.noreply.github.com>
2023-02-16 11:17:53 -0400

    Added backend

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+
+with app.app_context():
+    db.create_all()
