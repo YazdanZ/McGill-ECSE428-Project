2e466cb7f860cc312f49fc62b188c36b82c788f1
andrewlch <andrew.chirita@mail.mcgill.ca>
2023-02-20 02:00:14 -0500

    Updated data table relational errors

--- backend/app.py
+++ backend/app.py
@@ -15,31 +15,34 @@ class User(db.Model):
     mcgill_id = db.Column(db.Integer)
     password = db.Column(db.String(50))
     isDriver = db.Column(db.String(50))
-    cars = db.relationship("Car",back_populates="driver")
-    trips = db.relationship("Trip",back_populates="passengers")
+    cars = db.relationship("Car",back_populates="driver_relation")
+    trips = db.relationship("Trip",back_populates="passengers_relation")
 
 class Trip(db.Model):
     trip_id = db.Column(db.Integer, primary_key=True)
     distance_km = db.Column(db.Integer)
     passengers = db.Column(db.String(50), db.ForeignKey('user.email'), nullable=True)
+    passengers_relation = db.relationship("User", back_populates="trips")
     vehicle = db.Column(db.Integer, db.ForeignKey('car.car_id'), nullable=False)
+    vehicle_relation = db.relationship("Car", back_populates="trips")
     pickup_location = db.Column(db.String(100), db.ForeignKey('address.address_id'), nullable=False)
     dropoff_location = db.Column(db.String(100), db.ForeignKey('address.address_id'), nullable=False)
-    pickup_address = db.relationship("Address", back_populates="pickup_trips")
-    dropoff_address = db.relationship("Address", back_populates="dropoff_trips")
+    pickup_address = db.relationship("Address", back_populates="pickup_trips", foreign_keys=[pickup_location])
+    dropoff_address = db.relationship("Address", back_populates="dropoff_trips", foreign_keys=[dropoff_location])
     
 class Car(db.Model):
     car_id = db.Column(db.Integer, primary_key=True)
     driver = db.Column(db.Integer, db.ForeignKey('user.email'), nullable=False)
-    trips = db.relationship("Trip",back_populates="vehicle")
+    driver_relation = db.relationship("User", back_populates="cars")
+    trips = db.relationship("Trip",back_populates="vehicle_relation")
     fuel_consumption = db.Column(db.Integer) #km per gallon or smth
     seats = db.Column(db.Integer)
     
 class Address(db.Model):
     city = db.Column(db.String(100))
     address_line_1 = db.Column(db.String(200))
-    pickup_trips = db.relationship("Trip",back_populates="pickup_location", uselist=False)
-    dropoff_trips = db.relationship("Trip",back_populates="dropoff_location", uselist=False)
+    pickup_trips = db.relationship("Trip", back_populates="pickup_address", uselist=False, foreign_keys="Trip.pickup_location")
+    dropoff_trips = db.relationship("Trip", back_populates="dropoff_address", uselist=False, foreign_keys="Trip.dropoff_location")
     postal_code = db.Column(db.String(50))
     address_id = db.Column(db.Integer, primary_key=True)
 
@@ -79,22 +82,21 @@ def createTrip():
     db.session.commit()
     return "200"
 
-#include passenger id parameter
 @app.route("/getTrip", methods = ['GET'])
 def getTrip():
-    passenger_id = request.args.get('passengerId')
-    trip = Trip.query.filter(Trip.passengers.any(id=passenger_id)).first()
+    user_email = request.args.get('userEmail')
+    trip = Trip.query.filter(Trip.passengers.any(email=user_email)).first()
 
     if trip:
-        driver = User.query.filter_by(id=trip.driver_id).first() #not sure if we should have driver and passenger classes
+        driver = User.query.filter_by(email=trip.vehicle.driver).first()
         driver_name = driver.name
-        driver_vehicle = driver.vehicle
-        pickup_location = trip.pickup_location
-        dropoff_location = trip.dropoff_location
+        driver_vehicle = trip.vehicle.car_id
+        pickup_location = trip.pickup_address.address_line_1 + ", " + trip.pickup_address.city
+        dropoff_location = trip.dropoff_address.address_line_1 + ", " + trip.dropoff_address.city
         distance = trip.distance_km
-        tripId = trip.trip_id
-        #duration = calculate_duration(trip.distance_km) #find a way to calculate duration
-        #cost = calculate_cost(trip.distance_km, trip.fuel_consumption) #find a way to calculate cost
+        trip_id = trip.trip_id
+        fuel_consumption = trip.vehicle.fuel_consumption
+        available_seats = trip.vehicle.seats - len(trip.passengers)
 
         return {
             'driver_name': driver_name,
@@ -102,9 +104,9 @@ def getTrip():
             'pickup_location': pickup_location,
             'dropoff_location': dropoff_location,
             'distance': distance,
-            'tripId': tripId
-            #'duration': duration,
-            #'cost': cost
+            'trip_id': trip_id,
+            'fuel_consumption': fuel_consumption,
+            'available_seats': available_seats
         }
     else:
         return 'You are currently not signed up for any trip'
