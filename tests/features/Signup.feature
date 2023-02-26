05f6a084f795a849d6b76950519cfd1d1bdd0619
himelsaha29 <himelsaha29@live.com>
2023-02-26 13:09:30 -0500

    All steps passing

--- tests/features/Signup.feature
+++ tests/features/Signup.feature
@@ -9,8 +9,25 @@ Feature: Sign up for an account
 
 
 
-#    Scenario: Invalid email provided
-#        Given I am on the sign-up page
-#        And I provide an invalid email address
-#        And I click the "Submit" button
-#        Then I should see an error message indicating that the email address is invalid
+    Scenario: Email already exists
+        Given I am on the sign-up page
+        Given an account exists in the system with email 'johndoe@yahoo.com'
+        When I attempt to create an account with email 'johndoe@yahoo.com'
+        And I click the 'Submit' button
+        Then I should see an error message indicating email already exists in the system
+
+
+    Scenario: McGill ID already exists
+        Given I am on the sign-up page
+        Given an account exists in the system with mcgill id '24356'
+        When I attempt to create an account with mcgill id '24356'
+        And I click the 'Submit' button
+        Then I should see an error message indicating mcgill id already exists in the system
+
+
+
+    Scenario: Invalid email provided
+        Given I am on the sign-up page
+        When I attempt to create an account with an invalid email address
+        And I click the 'Submit' button
+        Then I should see an error message indicating that the email address is invalid
--- tests/steps/steps.py
+++ tests/steps/steps.py
@@ -2,8 +2,9 @@ from behave import given, when, then
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
+import json
+
 
 @given("I am on the sign-up page")
 def step_impl(context):
@@ -16,9 +17,9 @@ def step_impl(context):
     name_field = context.browser.find_element(By.NAME, 'name')
     name_field.send_keys('John Doe')
     email_field = context.browser.find_element(By.NAME, 'email')
-    email_field.send_keys('himel.saha@mal.mcgill.ca')
+    email_field.send_keys('himel.saha@ml.mcgill.ca')
     id_field = context.browser.find_element(By.NAME, 'mcgill_id')
-    id_field.send_keys('26013456')
+    id_field.send_keys('2601356')
     password_field = context.browser.find_element(By.NAME, 'password')
     password_field.send_keys('password123')
     checkbox = context.browser.find_element(By.NAME, 'checkbox')
@@ -27,9 +28,9 @@ def step_impl(context):
 
 @when("I click the 'Submit' button")
 def step_impl(context):
-    sign_up_button = context.browser.find_element(By.CLASS_NAME, 'button-container')
+    sign_up_button = context.browser.find_element(
+        By.CLASS_NAME, 'button-container')
     sign_up_button.click()
-
 
 @then("I should see a success message")
 def step_impl(context):
@@ -38,26 +39,100 @@ def step_impl(context):
     assert response.text == 'User successfully created'
 
 
-@when("I provide an invalid email address")
+@given("an account exists in the system with email 'johndoe@yahoo.com'")
 def step_impl(context):
-    name_field = context.browser.find_element_by_name('emainame')
+    payload = {
+        "name": "John Doe",
+        "email": "johndoe@yahoo.com",
+        "mcgill_id": "260123",
+        "password": "password123",
+        "isDriver": "True"
+    }
+
+    json_payload = json.dumps(payload)
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    requests.post('http://localhost:5000/signup/', headers=headers, data=json_payload)
+
+@when("I attempt to create an account with email 'johndoe@yahoo.com'")
+def step_impl(context):
+    name_field = context.browser.find_element(By.NAME, 'name')
     name_field.send_keys('John Doe')
-    email_field = context.browser.find_element_by_name('email')
-    email_field.send_keys('my email')
-    id_field = context.browser.find_element_by_name('mcgill_id')
-    id_field.send_keys('260123456')
-    password_field = context.browser.find_element_by_name('password')
+    email_field = context.browser.find_element(By.NAME, 'email')
+    email_field.send_keys('johndoe@yahoo.com')
+    id_field = context.browser.find_element(By.NAME, 'mcgill_id')
+    id_field.send_keys('26013156')
+    password_field = context.browser.find_element(By.NAME, 'password')
     password_field.send_keys('password123')
-    checkbox = context.browser.find_element_by_name('checkbox')
+    checkbox = context.browser.find_element(By.NAME, 'checkbox')
+    checkbox.click()
+
+@then("I should see an error message indicating email already exists in the system")
+def step_impl(context):
+    time.sleep(1)
+    response = context.browser.find_element(By.CLASS_NAME, 'Toastify__toast')
+    assert response.text == 'Email already exists'
+
+
+@given("an account exists in the system with mcgill id '24356'")
+def step_impl(context):
+    payload = {
+        "name": "Jane Doe",
+        "email": "janedoe@yahoo.com",
+        "mcgill_id": "24356",
+        "password": "password123",
+        "isDriver": "True"
+    }
+
+    json_payload = json.dumps(payload)
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    requests.post('http://localhost:5000/signup/', headers=headers, data=json_payload)
+
+@when("I attempt to create an account with mcgill id '24356'")
+def step_impl(context):
+    name_field = context.browser.find_element(By.NAME, 'name')
+    name_field.send_keys('Jane Doe')
+    email_field = context.browser.find_element(By.NAME, 'email')
+    email_field.send_keys('janedoe2@yahoo.com')
+    id_field = context.browser.find_element(By.NAME, 'mcgill_id')
+    id_field.send_keys('24356')
+    password_field = context.browser.find_element(By.NAME, 'password')
+    password_field.send_keys('password123')
+    checkbox = context.browser.find_element(By.NAME, 'checkbox')
     checkbox.click()
 
 
+@then("I should see an error message indicating mcgill id already exists in the system")
+def step_impl(context):
+    time.sleep(1)
+    response = context.browser.find_element(By.CLASS_NAME, 'Toastify__toast')
+    assert response.text == 'McGill ID already exists'
 
 
 
+@when("I attempt to create an account with an invalid email address")
+def step_impl(context):
+    name_field = context.browser.find_element(By.NAME, 'name')
+    name_field.send_keys('Jane Doe')
+    email_field = context.browser.find_element(By.NAME, 'email')
+    email_field.send_keys('janeyahoo')
+    id_field = context.browser.find_element(By.NAME, 'mcgill_id')
+    id_field.send_keys('24356')
+    password_field = context.browser.find_element(By.NAME, 'password')
+    password_field.send_keys('password123')
+    checkbox = context.browser.find_element(By.NAME, 'checkbox')
+    checkbox.click()
 
 @then("I should see an error message indicating that the email address is invalid")
 def step_impl(context):
-    error_message = context.browser.find_element_by_css_selector(
-        '.error-message')
-    assert error_message.text == "An account with this email address already exists."
+    time.sleep(1)
+    email_field = context.browser.find_element(By.NAME, 'email')
+    validation_message = email_field.get_attribute("validationMessage")
+    assert ('Please enter an email address' in validation_message)
