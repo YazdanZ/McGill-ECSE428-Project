f0f3e37bffe7e91e511f9ede72d77d2c5f60423a
Aidan Jackson <aidan.jackson@mail.mcgill.ca>
2023-03-09 14:34:14 -0500

    Modifed Home Button Path

--- frontend/src/components/menu_items/MenuBar.jsx
+++ frontend/src/components/menu_items/MenuBar.jsx
@@ -6,7 +6,7 @@ function MenuBarCustom({ logoutFunction }) {
         <nav className="menu-bar">
             <ul className="menu-items">
                 <li className="menu-item">
-                    <a href="/">Home</a>
+                    <a href="/user-info">Home</a>
                 </li>
                 <li className="menu-item logout">
                     <button onClick={logoutFunction}>Logout</button>
