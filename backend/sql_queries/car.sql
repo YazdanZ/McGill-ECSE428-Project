7a9185f771c816fa22718de055513ad8e09e2c7a
MAKS-1211 <mihir.kumar@mail.mcgill.ca>
2023-03-26 19:16:26 -0400

    All files added

--- backend/sql_queries/car.sql
+++ backend/sql_queries/car.sql
@@ -1,3 +1,3 @@
 -- SQLite
 INSERT INTO car_table (car_id, vehicle_description, fuel_consumption, seats, driver_id)
-VALUES (1, 'Toyota Corolla', 15, 5, 'andrew@mail.com');
+VALUES (1, 'Toyota Corolla', 15, 5, 'mihiranshul@gmail.com');
