f9a5c09822abac4513db09de8160827ffe5d0d89
anandamoyi <anandamoyi_saha@hotmail.com>
2023-03-31 22:43:29 -0400

    modified gherkins tests to be used in chrome

--- tests/steps/AddDriverSchedule.py
+++ tests/steps/AddDriverSchedule.py
@@ -2,6 +2,7 @@ from behave import *
 import behave_webdriver
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 import time
@@ -14,52 +15,80 @@ use_step_matcher("re")
 
 @given('I am on the add-driver-schedule page')
 def step_impl(context):
-    context.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-    context.driver.get('http://localhost:3000/add-driver-schedule')
+    # use this in firefox
+    # context.driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    # context.driver.get('http://localhost:3000/add-driver-schedule')
+    # Use this in chrome
+    context.behave_driver = behave_webdriver.Chrome(ChromeDriverManager().install())
+    context.behave_driver.get('http://localhost:3000/add-driver-schedule')
 
 @when('I fill in the start date, start time, and trip id')
 def step_impl(context):
-    context.driver.find_element_by_id('start_date').send_keys('30/03/2023')
-    context.driver.find_element_by_id('start_time').send_keys('10:00')
-    context.driver.find_element_by_id('trip_id').send_keys(1)
+    # use this in firefox
+    # context.driver.find_element_by_id('start_date').send_keys('30/03/2023')
+    # context.driver.find_element_by_id('start_time').send_keys('10:00')
+    # context.driver.find_element_by_id('trip_id').send_keys(1)
+    #use this in chrome
+    context.behave_driver.find_element_by_id('start_date').send_keys('30/03/2023')
+    context.behave_driver.find_element_by_id('start_time').send_keys('10:00')
+    context.behave_driver.find_element_by_id('trip_id').send_keys(1)
 
 @when('I click the Submit button')
 def step_impl(context):
-    #wait = WebDriverWait(context.driver, 50)
-    # context.driver.implicitly_wait(15)
-    # #element = context.driver.find_element(By.XPATH, '//*[@id="sub_btn"]')
-    # # element = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@id='sub_btn']")))
-    # # # Interact with the element
-    # # element.click()
-    # context.driver.find_element_by_id("bt").click()
-    wait = WebDriverWait(context.driver, 10)
+    # use this in firefox
+    # wait = WebDriverWait(context.driver, 10)
+    # element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "button-container")))
+    # element.click()
+    # use this in chrome
+    wait = WebDriverWait(context.behave_driver, 10)
     element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "button-container")))
     element.click()
 
 @then('I should see a success message')
 def step_impl(context):
-    WebDriverWait(context.driver, 3).until(EC.alert_is_present(),
+    # use this in firefox
+    # WebDriverWait(context.driver, 3).until(EC.alert_is_present(),
+    #                                               'Timed out waiting for PA creation ' +
+    #                                               'confirmation popup to appear.')
+    # alert = Alert(context.driver)
+    # assert alert.text == "Schedule added successfully!"
+    # context.driver.quit()
+    # use this in chrome
+    WebDriverWait(context.behave_driver, 3).until(EC.alert_is_present(),
                                                   'Timed out waiting for PA creation ' +
                                                   'confirmation popup to appear.')
-    alert = Alert(context.driver)
+    alert = Alert(context.behave_driver)
     assert alert.text == "Schedule added successfully!"
-    context.driver.quit()
+    context.behave_driver.quit()
 
 
 
 @when('I fill in a non-existing trip id')
 def step_impl(context):
-    context.driver.find_element_by_id('start_date').send_keys('30/03/2023')
-    context.driver.find_element_by_id('start_time').send_keys('10:00')
-    context.driver.find_element_by_id('trip_id').send_keys(5)
+    # use this in firefox
+    # context.driver.find_element_by_id('start_date').send_keys('30/03/2023')
+    # context.driver.find_element_by_id('start_time').send_keys('10:00')
+    # context.driver.find_element_by_id('trip_id').send_keys(5)
+    # use this in chrome
+    context.behave_driver.find_element_by_id('start_date').send_keys('30/03/2023')
+    context.behave_driver.find_element_by_id('start_time').send_keys('10:00')
+    context.behave_driver.find_element_by_id('trip_id').send_keys(5)
 
 
 
 @then('I should see an error message')
 def step_impl(context):
-    WebDriverWait(context.driver, 3).until(EC.alert_is_present(),
+    # use this in firefox
+    # WebDriverWait(context.driver, 3).until(EC.alert_is_present(),
+    #                                               'Timed out waiting for PA creation ' +
+    #                                               'confirmation popup to appear.')
+    # alert = Alert(context.driver)
+    # assert alert.text == "Unable to add schedule."
+    # context.driver.quit()
+    # use this in chrome
+    WebDriverWait(context.behave_driver, 3).until(EC.alert_is_present(),
                                                   'Timed out waiting for PA creation ' +
                                                   'confirmation popup to appear.')
-    alert = Alert(context.driver)
+    alert = Alert(context.behave_driver)
     assert alert.text == "Unable to add schedule."
-    context.driver.quit()
+    context.behave_driver.quit()
